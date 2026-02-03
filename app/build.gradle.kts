fdc868eb4636540759c879a7eb999b1419a5a3c7
adrencina <adr.encina@gmail.com>
2026-02-02 23:25:22 -0300

    blindaje de firebase.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.adrencina.enchu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 32
-        versionName = "2.5"
+        versionCode = 35
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
